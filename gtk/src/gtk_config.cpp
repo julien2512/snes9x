650cb595148ed1e2ee5d3f649c62fc585457f374
julien2512 <moquet.julien@gmail.com>
2018-01-04 22:43:30 +0100

    Restoring default cheat activation

--- gtk/src/gtk_config.cpp
+++ gtk/src/gtk_config.cpp
@@ -256,7 +256,7 @@ Snes9xConfig::load_defaults (void)
     Settings.DynamicRateControl = 1;
     Settings.DynamicRateLimit = 5;
     Settings.HDMATimingHack = 100;
-    Settings.ApplyCheats = 1;
+    Settings.ApplyCheats = 0;
 
 #ifdef NETPLAY_SUPPORT
     Settings.NetPlay = FALSE;
